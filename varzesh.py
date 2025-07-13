7c94a587a6ee4513115c9f937bf84ed930df0400
maryam Rezanezhad Farahmand <hastifrm81@gmail.com>
2025-07-14 02:48:11 +0330

    frist commit

---
+++ varzesh.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+web=requests.get("https://varzesh3.com")
+soup=BeautifulSoup(web.content)
+dvs=soup.find_all("div")
+dvs=soup.find_all("div",{"class":"news-main-list"})
+links=dvs[0].find_all("a")+dvs[1].find_all("a")
+ns=[]
+ms=[]
+for link in links:
+    if "https://video.varzesh3.com/video/" in link["href"]:
+        ns.append(" ")
+    else:
+        ms.append(link["href"]+"\n")
+print(len(ns))
+print(len(ms))
+for m in ms:
+    web=requests.get(m)
+    soup=BeautifulSoup(web.content)
+    dvs=soup.find_all("div",{"class":"news-main-image"})
+    if dvs != []:
+     img=dvs[0].find_all("img")[0]["src"]
+     n=img.strip().split("/")[-1].strip().split("?")[0]
+     f2=open("images/" +n ,"wb")
+     web=requests.get(img)
+     f2.write(web.content) 
+     f2.close()   # break
+# links=dvs[0]
